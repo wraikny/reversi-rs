7e2ed00b3e214dd20e042a0d6e22640e8da70562
wraikny <writtenknown@gmail.com>
2018-07-03 05:46:56 +0900

    not loop

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,12 @@ use reversi::cpu;
 
 fn main() {
     let setting = Setting {
-        black : PlayerType::Computer(1),
-        white : PlayerType::Computer(5),
+        black : PlayerType::Computer(3),
+        white : PlayerType::Computer(3),
         boardsize : (8, 8),
     };
 
-    let cpu_setting = cpu::Setting::new(5, 100, 20, 2, 10, 10);
+    let cpu_setting = cpu::Setting::new(5, 50, 20, 2, 10, 50);
     
-    for _ in 0..10 {
-        start(&setting, &cpu_setting, true);
-    }
+    start(&setting, &cpu_setting, true);
 }
